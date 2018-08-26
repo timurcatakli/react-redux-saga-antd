e1731436afee4d1c0638346b07c9ee71902134fa
Timur Catakli <timurcatakli@gmail.com>
2018-08-26 15:34:31 -0700

    01- React dev is ready to go…

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// inside src/index.js
+import React from 'react'
+import {render} from 'react-dom'
+
+const App = () => {
+ return <div>
+  <h1>React App Running!</h1>
+ </div>
+}
+
+render(
+ <App/>,
+ document.getElementById('root')
+)
